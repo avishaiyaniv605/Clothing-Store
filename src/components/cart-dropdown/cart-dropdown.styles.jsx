950d4fff13645b5b85c3739f81be3d96f2895e49
avishaiyaniv <avishaiy@post.bgu.il>
2019-10-26 18:26:57 +0300

    styled components

---
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import CustomButton from "../custom-button/custom-button.component";
+
+export const CartDropdownContainer = styled.div`
+  position: absolute;
+  width: 300px;
+  height: 440px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 90px;
+  right: 40px;
+  z-index: 5;
+  overflow: scroll;
+`;
+
+export const CartDropdownButton = styled(CustomButton)`
+  font-size: 1vw;
+  margin-top: auto;
+`;
+
+export const EmptyMessageContainer = styled.span`
+  letter-spacing: 1.3px;
+  font-size: 18px;
+  margin: 50px 0;
+  align-self: center;
+`;
+
+export const CartItemsContainer = styled.div`
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+`;
---
+++ src/pages/order-summery/OrderSummery.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ReactComponent as ShippmentLogo } from "../../assets/paper-plane.svg";
+import { ReactComponent as CardLogo } from "../../assets/card.svg";
+import { ReactComponent as DeliveryLogo } from "../../assets/delivery-truck.svg";
+import { ReactComponent as LocationLogo } from "../../assets/location.svg";
+import "./OrderSummery.scss";
+
+const OrderSummery = ({ location }) => {
+  const { card } = location.state.token;
+  console.log(location.state.token);
+  return (
+    <div className="order-container">
+      <div className="order-summery">
+        <div className="user-message">
+          <ShippmentLogo className="shippment-logo" />
+          <h1 className="title">
+            Congratulations {card.name}, Your order has been placed!
+          </h1>
+        </div>
+        <div className="order-details">
+          <div className="shipping right-bordered">
+            <LocationLogo className="logo" />
+            <span className="section-title">Shippment</span>
+            <span className="country">Country: {card.address_country}</span>
+            <span className="city">City: {card.address_city}</span>
+            <span className="street">Street: {card.address_line1}</span>
+            <span className="zip-code">Zipcode {card.address_zip}</span>
+          </div>
+          <div className="billing right-bordered">
+            <CardLogo className="logo" />
+            <span className="section-title">Billing</span>
+            <span className="country">Country: {card.address_country}</span>
+            <span className="city">City: {card.address_city}</span>
+            <span className="street">Street: {card.address_line1}</span>
+            <span className="zip-code">Zipcode {card.address_zip}</span>
+          </div>
+          <div className="method">
+            <DeliveryLogo className="logo" />
+            <span className="section-title">Shippment method</span>
+            <span>
+              US. Standard <br /> (normally 4-5 business days)
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSummery;
